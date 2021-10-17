56912841d02d1ab656d76b6591c41c3248683443
jjgilces <jjgilces@espol.edu.ec>
2021-10-17 17:56:20 -0500

    semana 1 2021

--- Clases/Semana 1-2/01 Variables.py
+++ Clases/Semana 1-2/01 Variables.py
@@ -10,38 +10,40 @@ print("\n")
 #Regla 1: Utiliza nombres de variables descriptivos*
 #Regla 2: Los nombres de variables empiezan por letras
 #Regla 3: No utilices espacios para separar nombres largos
-#Regla 4: Utiliza nombres cortos
-#Regla 5: Cada lenguaje tiene un estilo de escritura*
+
+K= 52 #esto no me dice
+
 
 #Crea una variable que contendrá tu nombre. Asigna tu nombre a la variable.
 
-nombre= "Johan"
+nombre= "Juan Gabriel" 
 
 #Muestra el contenido de la variable con tu nombre 
 print(nombre)
 
 
 #Ahora, crea una variable que contendrá el lugar de destino de tus vacaciones. Asigna el lugar que deseas ir de vacaciones.
-place= "Amsterdam" 
+
+destino_vacaciones = "Dubai"
 
 
 #Muestra el contenido de la variable con el lugar de destino de tus vacaciones
 
-print("Destino de las vacaciones: ", place  )
+print("Destino de las vacaciones: ", destino_vacaciones  )
 
 
 #Crea una variable con la fecha del viaje que deseas realizar. Asigna la fecha a la variable. Utiliza la notación con _
+fecha_viaje="21/Octubre/2021"
 
-fecha_viaje= "12 Julio del 2021"
 
 #Muestra el contenido de la variable con la fecha del viaje
 
 print("Fecha del viaje: ",fecha_viaje)
 
 #Crea la variable para el nombre de la aerolínea. Asigna el nombre de una aereolínea. Utiliza la notación con la primera letra de cada palabra en mayúsculas
-
-aerolinea= "Tames"
+aerolina = "Aerolina 1"
 #Muestra el nombre de la aereolínea
-print("¡Viajando por ",aerolinea,"!")
+
+print("¡Viajando por ",aerolina,"!")
 
 print("\n")
--- Clases/Semana 1-2/02 Tipo de Datos.py
+++ Clases/Semana 1-2/02 Tipo de Datos.py
@@ -7,35 +7,37 @@ print("************************")
 
 
 #Crea una variable a la que asignarás tu nombre completo, dos nombres y dos apellidos, como un texto. Muestra por pantalla el contenido de la variable.
-name= 'Johan Jair Gilces'
+name= "Johan Gilces"
 
-print("Mi nombre es :")
-print(name)
+print("Mi nombre es :", name)
+
 
 #Usa la variable fechaDeViaje a la que asignarás tu fecha de tu viaje como un texto. Muestra por pantalla el contenido de la variable.
 
-fechaDeViaje = "12 Julio"
+fechaDeViaje = "21/Octubre/2021"
 print("La fecha del viaje es :")
 print(fechaDeViaje)
 
 
 #Crea una variable a la que asignarás tu edad como un número entero. Muestra por pantalla el contenido de la variable.
 
-edad= 19
+edad= 20
 
-print(type(edad))
 print("La edad del viajero es:")
 print(edad, "años")
 
+#Como puedo saber el tipo de dato de una variable 
+print(type(edad))
+
 #Crea una variable a la que asignarás el costo del boleto, un número decimal: 925.68 dólares y muestra por pantalla el contenido de la variable.
 
-costoBoleto= 120.0
+costoBoleto= 500.23  #Los decimales se separan por PUNTO 
 print("El costo del boleto es", costoBoleto, "dolares")
 
 #Crea una variable a la que asignarás si viajarás solo o no. El valor será de tipo lógico (True o False). Muestra por pantalla el contenido de la variable viajoSolo.
 
-viajoSolo = True 
-print(type(viajoSolo))
+viajoSolo = True #los booleanes, siempre comienzan con MAYUSCULA 
+
 print("Estas vacaciones viajaré solo: ",  viajoSolo  )
 
 #Valores Lógicos comienzan con mayuscula (True y False)
