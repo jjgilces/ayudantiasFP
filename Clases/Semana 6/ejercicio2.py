7d1a545f2e693b73542dd8a5805ed5f9b2ba05bc
jjgilces <jjgilces@espol.edu.ec>
2022-11-17 15:10:10 -0500

    examen 1 parcial

--- Clases/Semana 6/ejercicio2.py
+++ Clases/Semana 6/ejercicio2.py
@@ -7,18 +7,41 @@ grasas =    [13.8,0.5,0.15,0.5,0.44,0.3,0.19,0.2,0.28]
 proteinas = [1.3,0.8,0.6,0.7,1.0,0.7,0.8,0.3,0.9]  
 #Seleccionar algunas frutas, tal que la suma del total de hidratos de las frutas seleccionadas sea menor que 30. El criterio de selección es empezando a tomar las frutas que tengan menor número de grasa. 
 #Presentar los nombres de las frutas seleccionadas con sus valores de hidratos.
-grasasOrdenadas= grasas.copy()
-grasasOrdenadas.sort()
-sumaHidratos=[]
-sumaFrutas=[]
-contadorHidratos=0 #15
-for i in grasasOrdenadas:
-    indiceGrase = grasas.index(i) #2
-    hidrato = hidratos[indiceGrase]   
-    if(contadorHidratos+hidrato<30):
-        contadorHidratos=contadorHidratos+ hidrato
-        fruta = frutas[indiceGrase]
-        print(fruta, hidrato)
+consumos=[]
+
+for i in range(len(energias)):
+    fruta= frutas[i]
+    energia= energias[i]
+    grasa= grasas[i]
+    #consumo = energeia *grasa
+    consumo = energia*grasa
+    consumos.append(consumo)
+print(consumos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# grasasOrdenadas= grasas.copy()
+# grasasOrdenadas.sort()
+# sumaHidratos=[]
+# sumaFrutas=[]
+# contadorHidratos=0 #15
+# for i in grasasOrdenadas:
+#     indiceGrase = grasas.index(i) #2
+#     hidrato = hidratos[indiceGrase]   
+#     if(contadorHidratos+hidrato<30):
+#         contadorHidratos=contadorHidratos+ hidrato
+#         fruta = frutas[indiceGrase]
+#         print(fruta, hidrato)
 
 
 
